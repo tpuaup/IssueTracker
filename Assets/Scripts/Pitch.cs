24397239c4a199d9b994f8e93898a989d0823a8b
Yimin <tpuaup@gmail.com>
2016-11-29 22:55:52 +0800

    寫完旋轉與球速設定

---
+++ Assets/Scripts/Pitch.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pitch : MonoBehaviour {
+	
+	public float speed = 100;
+	public float rotatingSpeed = 100f;
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody> ();
+		rb.maxAngularVelocity = 200 * 2 * Mathf.PI;
+		rb.angularVelocity = this.transform.right * -2 * Mathf.PI * rotatingSpeed;
+
+		rb.velocity = this.transform.forward * speed / 3.6f;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() 
+	{
+		
+	}
+}
